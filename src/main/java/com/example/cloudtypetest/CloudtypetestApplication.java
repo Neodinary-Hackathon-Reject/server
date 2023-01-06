41cf947bffd42fad43f35e124b8afe72b5612487
swa07016 <swa07016@naver.com>
2023-01-06 15:47:03 +0900

    initial commit

---
+++ src/main/java/com/example/cloudtypetest/CloudtypetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudtypetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudtypetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudtypetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudtypetest/CloudtypetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudtypetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudtypetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
