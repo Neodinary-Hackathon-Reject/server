c4b0a05077a6386565d47168db7e598c40a3ab45
swa07016 <swa07016@naver.com>
2023-01-07 13:10:19 +0900

    feat: Error convention

---
+++ src/main/java/com/example/cloudtypetest/exception/CustomException.java
@@ -0,0 +1,18 @@
+package com.example.cloudtypetest.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private ErrorCode errorCode;
+
+    public CustomException(String message, ErrorCode errorCode){
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
